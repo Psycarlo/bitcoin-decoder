c5e7bfd0bbc775b2cce72edbb2cfb85e240c7f08
contributor <contributors@users.noreply.github.com>
2026-06-28 01:22:20 +0200

    test: cover destination normalization and bolt fixtures

--- tests/fixtures/bolt11.ts
+++ tests/fixtures/bolt11.ts
@@ -1,13 +1,21 @@
 export const bolt11Invoices = {
   mainnet: {
     valid:
-      'lnbc10u1p5et0xtpp5lmuztz4yunmruy30a3xqgw4ng8m2m8vzx5af48f85t9ymu3gr0esdq823jhxaqcqzzsxqrrsssp5lc3sp7932r379kcmnat4c20cgudp4vhlzwsqjqcvw0gqw27pjv6s9qxpqysgq4a5zpt7227kz33qe4kx3wrzvm2m88nchqq5sjd4p8ezjfpdcxlwy6pw39qwhym7sxesxazyz6ezjv3l7r62musv6jsdn8aqqjw6r50qp32unnk'
+      'lnbc10u1p5et0xtpp5lmuztz4yunmruy30a3xqgw4ng8m2m8vzx5af48f85t9ymu3gr0esdq823jhxaqcqzzsxqrrsssp5lc3sp7932r379kcmnat4c20cgudp4vhlzwsqjqcvw0gqw27pjv6s9qxpqysgq4a5zpt7227kz33qe4kx3wrzvm2m88nchqq5sjd4p8ezjfpdcxlwy6pw39qwhym7sxesxazyz6ezjv3l7r62musv6jsdn8aqqjw6r50qp32unnk',
+    metadata: {
+      amount: 1000,
+      description: 'Test'
+    }
   },
   testnet: {
     valid:
       'lntbs10u1p5et0y3sp5cp3sp45t3hh84t08gcf49ye8athryy36vjym5q0r65sjxen5qkeqpp57m9wees47jhe83cjvtyaln0lpfne90scfvyhk630eklrpnuvfgqqdq4g9exkgznw3hhyefqyvensxqzjccqp2rzjq2v454h7kjlfx9c6kcfeprd4d7lsn4cmhsngyuvmx9pr6lmepgu0cpzs3yqqqtqqqqqqqqqpqqqqqzsqqc9qxpqysgqsxudq7yy8nq4lmct42eq6rhq4rl76w2u89w35nas4djcdyw4dvz9qs40ta8s9nu8sypwzr6hpsdelgaurn8nw0mr69nnmke7nmma25sqnxeyj0',
     uppercase:
-      'LNTBS10U1P5ET0Y3SP5CP3SP45T3HH84T08GCF49YE8ATHRYY36VJYM5Q0R65SJXEN5QKEQPP57M9WEES47JHE83CJVTYALN0LPFNE90SCFVYHK630EKLRPNUVFGQQDQ4G9EXKGZNW3HHYEFQYVENSXQZJCCQP2RZJQ2V454H7KJLFX9C6KCFEPRD4D7LSN4CMHSNGYUVMX9PR6LMEPGU0CPZS3YQQQTQQQQQQQQQPQQQQQZSQQC9QXPQYSGQSXUDQ7YY8NQ4LMCT42EQ6RHQ4RL76W2U89W35NAS4DJCDYW4DVZ9QS40TA8S9NU8SYPWZR6HPSDELGAURN8NW0MR69NNMKE7NMMA25SQNXEYJ0'
+      'LNTBS10U1P5ET0Y3SP5CP3SP45T3HH84T08GCF49YE8ATHRYY36VJYM5Q0R65SJXEN5QKEQPP57M9WEES47JHE83CJVTYALN0LPFNE90SCFVYHK630EKLRPNUVFGQQDQ4G9EXKGZNW3HHYEFQYVENSXQZJCCQP2RZJQ2V454H7KJLFX9C6KCFEPRD4D7LSN4CMHSNGYUVMX9PR6LMEPGU0CPZS3YQQQTQQQQQQQQQPQQQQQZSQQC9QXPQYSGQSXUDQ7YY8NQ4LMCT42EQ6RHQ4RL76W2U89W35NAS4DJCDYW4DVZ9QS40TA8S9NU8SYPWZR6HPSDELGAURN8NW0MR69NNMKE7NMMA25SQNXEYJ0',
+    metadata: {
+      amount: 1000,
+      description: 'Ark Store #38'
+    }
   },
   signet: {},
   regtest: {}
